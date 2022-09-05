e16395951371c02fc99d66b61cc4585666457ae1
Marina Silnova <marusia421@mail.ru>
2022-09-05 23:53:34 +0300

    Решение задачи №1

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 1
+
+Console.WriteLine ("input first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("input second number");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("Ответ на задачу 1 - max = " + Convert.ToInt32(a));
+}
+else
+{
+    Console.WriteLine("Ответ на задачу 1 - max = " + Convert.ToInt32(b));
+}
