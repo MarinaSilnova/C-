812f4566a3f9472ed85ae0c3c77f49366942cc20
Marina Silnova <marusia421@mail.ru>
2022-09-06 00:22:17 +0300

    Решение задачи №3

---
+++ ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿// задача 3
+Console.WriteLine ("input number");
+int r = Convert.ToInt32(Console.ReadLine());
+if(r % 2 == 0)
+{
+   Console.WriteLine("Ответ на задачу 3 - Чилсо четное" ); 
+}
+else 
+{
+    Console.WriteLine("Ответ на задачу 3 - Чилсо нечетное" );
+}
+
