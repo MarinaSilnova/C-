3bb333d1c77264404338c8ffe5f66ce5aee7614f
Marina Silnova <marusia421@mail.ru>
2022-09-06 00:18:04 +0300

    Решение задачи №2

---
+++ ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача 2
+Console.WriteLine ("input first number");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("input second number");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("input third number");
+int thirdnumber = Convert.ToInt32(Console.ReadLine());
+int max = firstnumber;
+if(max < secondnumber)
+{
+    max = secondnumber;
+}
+if(max < thirdnumber)
+{
+    max = thirdnumber;  
+}
+Console.WriteLine("Ответ на задачу 2 max = " + Convert.ToInt32(max));
